c2f59c235ddd8c78ca2ba2422eda86d513077494
breakfastattwelve <l3ooksharkel2@gmail.com>
2025-09-28 17:44:56 +0700

    Add Styling, Fetching Realtime Data, Display Details

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,7 +1,88 @@
+import sunny from "../assets/images/sunny.png";
+import cloudy from "../assets/images/cloudy.png";
+import rainy from "../assets/images/rainy.png";
+import snowy from "../assets/images/snowy.png";
+import { useState, useEffect } from "react";
+
 function WeatherApp() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const api_key = "867b0c8cdc2ed637ff1834fbeabe0cd2";
+
+  useEffect(() => {
+    const fetchDefaultWeather = async () => {
+        const defaultLocation = "Bangkok"
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=${api_key}`
+        const res = await fetch(url)
+        const defaultData = await res.json()
+        setData(defaultData)
+    }
+    fetchDefaultWeather()
+}, [])
+
+  const search = async () => {
+    if(location.trim() !== ""){
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`;
+        const res = await fetch(url);
+        const searchData = await res.json();
+        console.log(searchData);
+        setData(searchData);
+        setLocation("");
+    }
+
+  };
+
+  function handleKeyDown(e){
+    if(e.key === "Enter"){
+        search()
+    }
+  }
+
+  function handleInputChange(e) {
+    setLocation(e.target.value);
+  }
   return (
-    <div>WeatherApp</div>
-  )
+    <div className="container">
+      <div className="weather-app">
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location-dot"></i>
+            <div className="location">{data.name}</div>
+          </div>
+          <div className="search-bar">
+            <input
+              type="text"
+              value={location}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+              placeholder="Enter Location"
+            />
+            <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
+          </div>
+        </div>
+        <div className="weather">
+          <img src={sunny} alt="sunny" />
+          <div className="weather-type">{data.weather? data.weather[0].main : null}</div>
+          <div className="temp">{data.main ? `${Math.floor(data.main.temp)}°` : null}</div>
+        </div>
+        <div className="weather-date">
+          <p>Fri, 3 May</p>
+        </div>
+        <div className="weather-data">
+          <div className="humidity">
+            <div className="data-name">Humidity</div>
+            <i className="fa-solid fa-droplet"></i>
+            <div className="data">{data.main ? `${data.main.humidity}%` : null}</div>
+          </div>
+          <div className="wind">
+            <div className="data-name">Wind</div>
+            <i className="fa-solid fa-wind"></i>
+            <div className="data">{data.wind ? `${data.wind.speed}km/h` : null}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default WeatherApp
+export default WeatherApp;
